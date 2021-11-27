dfde7ed65ab786694bec55598d475126b93fd361
amalmys <amalmysore@gmail.com>
2021-11-27 08:20:06 +0530

    Add files via upload
    grocery list via file upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import "./App.css";
+import React from "react";
+import BasicLayout from "./layouts/BasicLayout";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+       <Router>
+        <Switch>
+          <Route path="/" component={BasicLayout} />
+          <Route path="/view-list"  component={BasicLayout} />
+          <Route path="/edit-list" component={BasicLayout} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
